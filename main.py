da1f3a35f910d189b878b675a50cbabffc8e26d5
sebasukodo <sebastianlein@web.de>
2025-09-16 10:43:19 +0200

    adding character counting function to stats and modify main.py accordingly

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_characters
 
 def get_book_text(filepath):
     with open(filepath) as path:
         return path.read()
 
 def main():
-    num_words = get_num_words(get_book_text("books/frankenstein.txt"))
+    frankenstein = get_book_text("books/frankenstein.txt")
+    num_words = get_num_words(frankenstein)
+    num_characters = get_num_characters(frankenstein)
     print(f"{num_words} words found in the document")
+    print(num_characters)
 
 main()
