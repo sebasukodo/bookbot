9ff689c9f9d9d6cfd34e3d3b883753dcc9cf6f7c
sebasukodo <github@sebastianlein.de>
2025-09-16 10:43:19 +0200

    adding character counting function to stats and modify main.py accordingly

--- stats.py
+++ stats.py
@@ -1,2 +1,17 @@
 def get_num_words(text):
     return len(text.split())
+
+def get_num_characters(text):
+    text = text.lower()
+    dictionary = {}
+
+    for letter in text:        
+        if letter in dictionary:
+            dictionary[letter] += 1
+        else:
+            dictionary[letter] = 1
+
+    return dictionary
+
+if __name__ == "__main__":
+    print(get_num_characters("Hello my friend! I am counting characters."))
