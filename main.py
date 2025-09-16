b8eb16174d1e25a26410b32c679743044f53c3eb
sebasukodo <github@sebastianlein.de>
2025-09-16 10:27:51 +0200

    refactor code

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+from stats import get_num_words
+
 def get_book_text(filepath):
     with open(filepath) as path:
         return path.read()
 
-def number_of_words_in_string(text):
-    return len(text.split())
-
 def main():
-    num_words = number_of_words_in_string(get_book_text("books/frankenstein.txt"))
+    num_words = get_num_words(get_book_text("books/frankenstein.txt"))
     print(f"{num_words} words found in the document")
 
 main()
